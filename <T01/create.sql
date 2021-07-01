81264af020e24e71e239cad9f52a7e5f6b755d8f
renatak12 <54039830+renatak12@users.noreply.github.com>
2021-07-01 17:57:39 -0300

    Update create.sql

--- <T01/create.sql
+++ <T01/create.sql
@@ -4,68 +4,66 @@ drop table if exists trabalha cascade;
 drop table if exists gerente cascade;
 
 CREATE TABLE empregado(
-    cod_empregado NUMERIC(4) PRIMARY KEY NOT NULL,  
-    nome_empregado CHAR(100) NOT NULL,
-    rua CHAR(255) NOT NULL,
-    cidade CHAR(100) NOT NULL,
-    salario FLOAT NOT NULL
+	cod_empregado CHAR(4) NOT NULL, 
+	nome_empregado VARCHAR(100) NOT NULL, 
+	rua VARCHAR(255) DEFAULT NULL, 
+	cidade VARCHAR(50) DEFAULT NULL, 
+	salario FLOAT NOT NULL,
+	PRIMARY KEY (cod_empregado)
 );
 
-CREATE TABLE companhia (
-    cod_companhia NUMERIC(4) PRIMARY KEY NOT NULL,
-    cnpj CHAR(12) UNIQUE NOT NULL,  
-    nome_companhia CHAR(100) NOT NULL,
-    cidade CHAR(100) NOT NULL
+CREATE TABLE companhia(
+	cod_companhia CHAR(4) NOT NULL ,
+	cnpj CHAR(12) NOT NULL, 
+	nome_companhia VARCHAR(100) DEFAULT NULL, 
+	cidade VARCHAR(50) DEFAULT NULL,
+	PRIMARY KEY (cod_companhia),
+	UNIQUE (cnpj)
 );
 
-CREATE TABLE trabalha (
-    cod_empregado NUMERIC(4) PRIMARY KEY NOT NULL,  
-    cod_companhia CHAR(12) NOT NULL,
-    CONSTRAINT fk_empregado FOREIGN KEY (cod_empregado)
-        REFERENCES empregado(cod_empregado)
-        ON DELETE RESTRICT ON UPDATE CASCADE,
- 	CONSTRAINT fk_cod_companhia FOREIGN KEY (cod_companhia)
-        REFERENCES companhia(cnpj)
-        ON DELETE RESTRICT ON UPDATE CASCADE
+
+CREATE TABLE trabalha(
+	cod_empregado CHAR(4) NOT NULL, 
+	cod_companhia CHAR(4) NOT NULL ,
+	PRIMARY KEY (cod_empregado, cod_companhia),
+	FOREIGN KEY (cod_empregado) REFERENCES empregado(cod_empregado),
+	FOREIGN KEY (cod_companhia) REFERENCES companhia(cod_companhia)
 );
 
-CREATE TABLE gerente (
-    cod_empregado NUMERIC(4)PRIMARY KEY NOT NULL,  
-    cod_companhia CHAR(12) NOT NULL,
-  	CONSTRAINT fk_empregado FOREIGN KEY (cod_empregado)
-       	REFERENCES empregado(cod_empregado)
-        ON DELETE RESTRICT ON UPDATE CASCADE,
-  	CONSTRAINT fk_companhia FOREIGN KEY (cod_companhia)
-       	REFERENCES companhia(cnpj)
-        ON DELETE RESTRICT ON UPDATE CASCADE
+CREATE TABLE gerente(
+	cod_empregado CHAR(4) NOT NULL, 
+	cod_companhia CHAR(4) NOT NULL,
+	PRIMARY KEY (cod_empregado, cod_companhia),
+	FOREIGN KEY (cod_empregado) REFERENCES empregado(cod_empregado),
+	FOREIGN KEY (cod_companhia) REFERENCES companhia(cod_companhia)
 );
 
-INSERT INTO empregado (cod_empregado, nome_empregado, rua, cidade, salario)
-	VALUES 
-		(9493, 'Matheus', 'Rua Generina Vale', 'caicó', 12000),
-		(9494, 'Ana', 'Pedro Velho', 'caicó', 1200),
-		(9495, 'Maria', 'Renato Dantas', 'caicó', 2500),
-		(9496, 'João', 'João Damasceno', 'caicó', 1200)
-;
-
-INSERT INTO companhia (cod_companhia, cnpj, nome_companhia, cidade)
+INSERT INTO companhia(
+	cod_companhia, cnpj, nome_companhia, cidade)
 	VALUES
-		(1434,'947374837733', 'Soft Sell', 'caicó'),
-		(4321,'947374837735', 'Soft Sell', 'caicó'),
-		(1233,'777333222112', 'Other', 'caicó'),
-		(1254,'777333222111', 'Other', 'caicó')
-;
+	(1,456845455560, 'prime','Caico'),
+	(2,947374837735, 'Soft Sell', 'Caico'),
+	(3,987444343431, 'Other', 'Natal'),
+    	(4,777333222111, 'Other2', 'Natal');
+	
+INSERT INTO empregado(
+	cod_empregado, nome_empregado, rua, cidade, salario) 
+	VALUES
+	(1,'Matheus', 'Rua Generina Vale', 'Caico', 12000),
+	(2,'Ana','Pedro Velho', 'Natal', 1200),
+	(3,'Maria', 'Rua Generina Vale', 'Caico', 2500),
+	(4,'João', 'João Damasceno', 'Caico', 1200);
 
-INSERT INTO trabalha (cod_empregado, cod_companhia)
-	VALUES 
-		(9493, '947374837733'),
-		(9495, '777333222112'),
-		(9494, '947374837735'),
-		(9496, '777333222111')
-;
+INSERT INTO trabalha
+	(cod_empregado, cod_companhia)
+	VALUES
+	(1,2),
+    	(2,1),
+    	(3,2),
+    	(4,2);
 
-INSERT INTO gerente (cod_empregado, cod_companhia)
-	VALUES 
-		(9493, '947374837733'),
-		(9495, '777333222112')
-;
+INSERT INTO gerente(
+	cod_empregado, cod_companhia)
+	VALUES
+	(1,2),
+    	(2,1);
